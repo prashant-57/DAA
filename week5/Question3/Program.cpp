6fb56f3073877e469c1aedc03406348997268951
prashant-57 <prashantdhoundiyal57@gmail.com>
2021-09-23 12:02:40 -1200

    Common Elements Between Arrays

---
+++ week5/Question3/Program.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n;
+    int arr1[n];
+    for(int i=0; i<n; ++i)
+    {
+        cin>>arr1[i];
+    }
+    cin>>m;
+    int arr2[m];
+    for(int i=0; i<m; ++i)
+    {
+        cin>>arr2[i];
+    }
+
+    for(int i=0; i<n; ++i)
+    {
+        for(int j=0; j<m; ++j)
+        {
+            if(arr1[i] == arr2[j])
+            {
+                cout<<arr1[i]<<" ";
+                arr2[j] = NULL;
+                break;
+            }
+        }
+    }
+    cout<<endl;
+    return 0;
+}
